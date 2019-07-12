be602879ad9bfee6417d35cac9aa78c1be4283f3
guoqiuchi <50602892+guoqiuchi@users.noreply.github.com>
2019-07-12 10:39:30 +0800

    查找学号
    通过爬虫实现统计

---
+++ 查找学号.py
@@ -0,0 +1,13 @@
+import random
+import datetime
+
+if __name__ == '__main__':
+  sid_list = list()
+  with open('students@python', encoding='utf8') as f:
+ n=0
+  for line in f:
+   sid = line.split(',')[1]
+     sid=sid.strip
+     if sid[:]==学号：201811123004:
+         n=n+1
+       print (n)
